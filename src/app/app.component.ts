af7384385be371655198dc5f8861688a62bbba94
Andrei Antal <antal.andrei@icloud.com>
2019-06-05 21:51:56 +0300

    Starter Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ngi-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
